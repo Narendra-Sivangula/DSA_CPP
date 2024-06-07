8b5d99a391b75e86fb6d4c4c13dd9c037fc68e56
Narendra <narendra115c@gmail.com>
2024-06-07 16:44:33 +0530

    C++_Notes

--- PrimeOrNot.cpp
+++ PrimeOrNot.cpp
@@ -2,21 +2,23 @@
 #include<cmath>
 using namespace std;
 
+bool isPrime(int n){
+    if(n <= 1) return false;
+    if(n <= 3) return true;
+
+    if(n%2 == 0 || n%3 == 0) return false;
+    for(int i=5;i<sqrt(n);i+=6)
+    if(n%i == 0 || n%(i+2) == 0) return false;
+
+    return true;
+}
+
 int main(){
     int n;
-    int i;
     cin>>n;
-    int flag = 0;
-    for(i=2;i<=sqrt(n);i++){
-        if(n%i == 0){
-            cout<<"Not Prime"<<endl;
-            flag = 1;
-            break;
-        }
-    }
-    
-    if(flag==0){
-        cout<<"Prime"<<endl;
-    }
+    if(isPrime(n))
+    cout<<"Prime"<<endl;
+    else
+    cout<<"Not Prime"<<endl;
     return 0;
 }
